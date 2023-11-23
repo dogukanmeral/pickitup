ab05a95c84d70fd6645ea702439f23eeb8dbdf9d
dogumer2@gmail.com <dogumer2@gmail.com>
2023-11-23 15:26:31 +0300

    add folder creation part

--- pickitup/Main.py
+++ pickitup/Main.py
@@ -18,13 +18,19 @@ def main():
     files = os.listdir(args.folder)
     
     if args.organize_type == 't':
-        files_by_categories = by_type(files)
+        files_by_categories = by_type(files, args.folder)
     elif args.organize_type == 'd':
         files_by_categories = by_date(files, args.folder)
     elif args.organize_type == 's':
-        files_by_categories = by_size(files)
+        files_by_categories = by_size(files, args.folder)
 
     folders_to_create = [files_by_categories[file] for file in files_by_categories]
     folders_to_create = list(set(folders_to_create)) # delete duplicates
 
-    # FIND A CONSISTENT WAY TO CREATE PATHS
+    for folder_path in folders_to_create:
+        full_folder_path = os.path.join(args.folder, folder_path)
+        
+        if not is_folder_existing(full_folder_path):
+            create_folder(full_folder_path)
+
+    
