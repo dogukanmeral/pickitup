0f369867ec190a02f5a457c04826df6930df25ea
Dogukan Meral <dogukan.meral@yahoo.com>
2023-11-23 12:40:33 +0300

    optimize library imports and fix path bug

--- pickitup/categorize/by_date.py
+++ pickitup/categorize/by_date.py
@@ -1,4 +1,6 @@
-import os, calendar, time
+import os
+import calendar
+import time
 
 def file_mod_time(file_path):
 	return time.localtime(os.path.getmtime(file_path))
@@ -6,10 +8,10 @@ def file_mod_time(file_path):
 def file_mod_month_year(mod_time):
 	return calendar.month_abbr[mod_time.tm_mon], str(mod_time.tm_year)
 
-def by_date(files):
+def by_date(files, current_path):
 	files_by_mod_dates = {}
 
 	for file in files:
-		files_by_mod_dates[file] = " ".join(file_mod_month_year(file_mod_time(file)))
+		files_by_mod_dates[file] = " ".join(file_mod_month_year(file_mod_time(f"{current_path}\{file}")))
 
 	return files_by_mod_dates
